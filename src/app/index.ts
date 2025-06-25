e5a49953080c131d186bd020e912181439e91652
Michman <michmanvadik@gmail.com>
2025-06-25 14:37:14 +0300

    [feature-4] added redux

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,3 @@
 export { App } from './App';
+export { store } from './store/store';
+export type { RootState, AppDispatch } from './store/store';
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './app';
+import { Provider } from 'react-redux';
+import { App, store } from './app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
