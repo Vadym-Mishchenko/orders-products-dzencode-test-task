aae76265811d8c7ea7cea9eb90eacb55256f0029
Michman <michmanvadik@gmail.com>
2025-07-04 20:32:47 +0300

    [feature-37] fixed ws

--- server/socket-server.cjs
+++ server/socket-server.cjs
@@ -1,17 +1,17 @@
-const http = require('http');
-const { Server } = require('socket.io');
+// const http = require('http');
+// const { Server } = require('socket.io');
 
-const server = http.createServer();
-const io = new Server(server, { cors: { origin: '*' } });
+// const server = http.createServer();
+// const io = new Server(server, { cors: { origin: '*' } });
 
-let sessionCount = 0;
+// let sessionCount = 0;
 
-io.on('connection', (socket) => {
-  sessionCount++;
-  io.emit('sessionCount', sessionCount);
+// io.on('connection', (socket) => {
+//   sessionCount++;
+//   io.emit('sessionCount', sessionCount);
 
-  socket.on('disconnect', () => {
-    sessionCount = Math.max(0, sessionCount - 1);
-    io.emit('sessionCount', sessionCount);
-  });
-});
+//   socket.on('disconnect', () => {
+//     sessionCount = Math.max(0, sessionCount - 1);
+//     io.emit('sessionCount', sessionCount);
+//   });
+// });
